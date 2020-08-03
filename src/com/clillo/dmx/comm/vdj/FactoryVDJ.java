572ed322b448bc1c5455761f0d8748642390ad08
carlos <carlos@DESKTOP-BUHFA21>
2020-08-03 16:58:03 -0400

    nueva version

--- src/com/clillo/dmx/comm/vdj/FactoryVDJ.java
+++ src/com/clillo/dmx/comm/vdj/FactoryVDJ.java
@@ -5,18 +5,18 @@ public class FactoryVDJ {
 	private static VJReceptor ejecutor;
 	
 	static{
-		ejecutor = new VJReceptor();
-		ejecutor.setName("Thread Receptor Virtual DJ");
-		ejecutor.start();
+	//	ejecutor = new VJReceptor();
+	//	ejecutor.setName("Thread Receptor Virtual DJ");
+	//	ejecutor.start();
 	}
 	
 	public static void init(){}
 	
 	public static void agregaListener(VJListener listener){
-		ejecutor.agregaListener(listener);
+		//ejecutor.agregaListener(listener);
 	}
 	
 	public static void eliminaListener(VJListener listener){
-		ejecutor.eliminaListener(listener);
+		//ejecutor.eliminaListener(listener);
 	}
 }
